b441e3872b87d78f96d90b1a2fa9021d83ec0ad0
santosh <santoshchouhan2442@gmail.com>
2024-11-25 19:19:50 +0530

    configed old depencency, change in db

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = `mongodb://${username}:${password}@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    const URL = `mongodb+srv://${username}:${password}@cluster0.xxb75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
